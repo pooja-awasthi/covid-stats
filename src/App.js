aa05c4b543053b7fe3d4dfc690c43b4001632a56
Shubham Mishra <shubhamsag3@outlook.com>
2022-07-23 20:20:37 +0530

    updating remote repo

---
+++ src/App.js
@@ -0,0 +1,69 @@
+import React, { useState, useEffect } from "react";
+import Cards from "./Components/Cards/Cards.jsx";
+import ChartView from "./Components/Chart/Chart.jsx";
+import CountryPicker from "./Components/CountryPicker/CountryPicker.jsx";
+import styles from "./App.css";
+import { fetchData } from "./api";
+import SignIn from './Components/SignIn/SignIn.jsx';
+
+
+function App () {
+  const [viewData, setViewData] = useState({
+    data: {},
+    country: "",
+  });
+  const [isAuthenticated, setIsAuthenticated] = useState(null);
+
+  const handleSetAuthentication = () => {
+    console.log("inside handle authentication")
+    setIsAuthenticated(true);
+  }
+
+
+
+  useEffect(() => {
+    if (localStorage.getItem('user')) {
+      setIsAuthenticated(true);
+      const getData = async() => {
+        const fetchedData = await fetchData();
+        setViewData({ ...viewData, data: fetchedData });
+      }
+      getData();
+    } else {
+      setIsAuthenticated(false);
+      setViewData({
+        data: {},
+        country: "",
+      })
+    }
+  }, []);
+
+  const handleCountryChange = async (country) => {
+    const fetchedData = await fetchData(country);
+    setViewData({ data: fetchedData, country: country });
+  };
+    return (
+      <div className={styles.container}>
+      {isAuthenticated && (typeof viewData.data !== 'undefined') ? (
+        <div>
+          <text>
+            <b style={{textAlign: 'center'}}><strong>Global and Country Wise Cases of Corona Virus</strong></b>
+          </text>
+          <br />
+          <text>
+            <i>(For a particular select a Country from below)</i>
+          </text>
+          <br />
+          <br />
+          <CountryPicker handleCountryChange={handleCountryChange} />
+          <br />
+          <br />
+          <Cards data={viewData.data} country={viewData.country} />
+          <ChartView data={viewData.data} country={viewData.country} />
+        </div>) :
+        ( <SignIn handleAuthentication = {handleSetAuthentication} /> ) }
+      </div>
+    );
+}
+
+export default App;
---
+++ src/Components/SignIn/SignIn.jsx
@@ -0,0 +1,51 @@
+
+import React, { useEffect, useState } from "react";
+import FacebookLogin from 'react-facebook-login';
+import { GoogleLogin } from 'react-google-login';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import config from './config.json';
+import style from './SignIn.css'
+
+const SignIn = ({handleAuthentcation}) => {
+    const googleResponse = async (response) => {
+        console.log("response: ",response)
+
+        const tokenBlob = new Blob([JSON.stringify({access_token: response.accessToken}, null, 2)], {type : 'application/json'});
+        console.log("response: ",response)
+        localStorage.setItem({ user: tokenBlob});
+
+        handleAuthentcation();
+    };
+
+    const onFailure = (error) => {
+    console.log(error);
+    };
+    
+  return (
+    <div className={style.container}>
+        {/* <FacebookLogin
+        appId={config.FACEBOOK_APP_ID}
+        autoLoad={false}
+        fields="name,email,picture"
+        callback={this.facebookResponse}
+        /> */}
+        <Card sx={{ minWidth: 250 }}>     
+        <CardContent>
+            Login with SocialMedia
+        </CardContent>
+        <CardActions>
+        <GoogleLogin
+            clientId={config.GOOGLE_CLIENT_ID}
+            buttonText="Login"
+            onSuccess={googleResponse}
+            onFailure={onFailure}
+        />
+        </CardActions>
+        </Card>
+    </div>
+  );
+}
+
+export default SignIn;
